203079cd9852cf8e78e44aa707dda6870eb9617c
demidraksin <greatraksin@icloud.com>
2021-05-08 18:34:46 +0300

    Adding dice rolling📱

--- 04_dicerolling.py
+++ 04_dicerolling.py
@@ -3,4 +3,11 @@ from thinkbayes2 import Suite
 
 class Dice(Suite):
     def Likelihood(self, data, hypo):
-
+        """Считает вероятность данных по гипотезе
+        hypo: число граней на игральной кости
+        data: число бросков кубика
+        """
+        if hypo < data:
+            return 0
+        else:
+            return 1.0/hypo
