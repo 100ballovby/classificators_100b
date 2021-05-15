7c78b7023781e216f9628c7b3b9e4061e589f249
demidraksin <greatraksin@icloud.com>
2021-05-15 20:13:05 +0300

    Adding a train data for spam filterℹ️

--- 1505-spam-filter/00_worker.py
+++ 1505-spam-filter/00_worker.py
@@ -1,8 +1,81 @@
-train_spam = ['send yor password', 'send us your account details',
-              'subscribe to our newsletter', 'you have received a large inheritance']
+train_spam = ['send your password', 'send us your account details',
+              'subscribe to our newsletter', 'you have received a large inheritance',
+              'review our website', 'send and sms', 'subscribe newsletter']
 train_ham = ['your activity report', 'benefits physical activity', 'the important vows',
-             'view you stats']
+             'view you stats', 'renew subscription']
 test_email = {
     'spam': ['renew your password', 'renew your account'],
     'ham': ['benefits of your account', 'the importance of physical activity']
 }
+
+
+# сделаем словарь уникальных слов, которые появлялись в спам-сообщениях (о которых известно)
+spam_dict = []
+for sentence in train_spam:
+    sentence_list = sentence.split()
+    for word in sentence_list:
+        spam_dict.append(word)
+
+unique_spam_dict = list(dict.fromkeys(spam_dict))
+print(unique_spam_dict)
+
+dict_spamicity = {}
+for word in unique_spam_dict:
+    emails_with_word = 0
+    for sentence in train_spam:  # перебор всех спамных сообщений
+        if word in sentence:  # если слово встречается в спамном сообщении
+            emails_with_word += 1  # "посчитать" это письмо
+    total_spam = len(train_spam)
+    spamicity = (emails_with_word + 1) / (total_spam + 2)
+    dict_spamicity[word.lower()] = spamicity
+
+print(dict_spamicity)
+
+
+# рассчитываю хамисити (чистоту) для слов из тренировочного набора
+ham_dict = []
+for sentence in train_ham:
+    sentence_list = sentence.split()
+    for word in sentence_list:
+        ham_dict.append(word)
+
+unique_ham_dict = list(dict.fromkeys(ham_dict))
+print(unique_ham_dict)
+
+dict_hamicity = {}
+for word in unique_ham_dict:
+    emails_with_word = 0
+    for sentence in train_ham:  # перебор всех хамных сообщений
+        if word in sentence:  # если слово встречается в хамном сообщении
+            emails_with_word += 1  # "посчитать" это письмо
+    total_ham = len(train_spam)
+    hamicity = (emails_with_word + 1) / (total_ham + 2)
+    dict_hamicity[word.lower()] = hamicity
+
+print(dict_hamicity)
+
+
+# считаю вероятность спама (априорная/классическая)
+prob_spam = len(train_spam) / (len(train_spam) + len(train_ham))
+print(prob_spam)
+# считаю вероятность хама (априорная/классическая)
+prob_ham = len(train_ham) / (len(train_spam) + len(train_ham))
+print(prob_ham)
+
+
+test = []
+for i in test_email['spam']:
+    test.append(i)
+
+for i in test_email['ham']:
+    test.append(i)
+
+# разбить имейлы на отдельные слова
+distinct_words_from_email = []
+for sentence in test:
+    sentence_as_list = sentence.split()
+    senten = []
+    for word in sentence_as_list:
+        senten.append(word)
+    distinct_words_from_email.append(senten)
+print(distinct_words_from_email)
