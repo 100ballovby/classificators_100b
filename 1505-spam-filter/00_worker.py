ecce0244cb7b9fa58d9a09538e6b33c105aa6262
demidraksin <greatraksin@icloud.com>
2021-05-15 18:50:11 +0300

    Adding a train data for spam filterℹ️

---
+++ 1505-spam-filter/00_worker.py
@@ -0,0 +1,8 @@
+train_spam = ['send yor password', 'send us your account details',
+              'subscribe to our newsletter', 'you have received a large inheritance']
+train_ham = ['your activity report', 'benefits physical activity', 'the important vows',
+             'view you stats']
+test_email = {
+    'spam': ['renew your password', 'renew your account'],
+    'ham': ['benefits of your account', 'the importance of physical activity']
+}
